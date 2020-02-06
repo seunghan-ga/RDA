2190bcec84e7ebb30e093fe5d61f85c1db5bb669
seunghan-ga <60493328+seunghan-ga@users.noreply.github.com>
2020-02-06 10:47:22 +0900

    Update test_t2.py

--- test_t2.py
+++ test_t2.py
@@ -22,61 +22,37 @@ if __name__ == "__main__":
     cols = true.columns.tolist()
     data = connector.select('select * from bearing where idx_date >= "2004-02-16 03:12:39";')
     all_data = connector.select('select * from bearing order by idx_date asc limit 10;')
-    connector.close()
+    connector.close()  
 
-    # print(true[cols[1:]].to_numpy().tolist())
-    # u, s, vt = np.linalg.svd(true[cols[1:]].to_numpy().tolist(), full_matrices=True)
-    # print(u.shape, s.shape, vt.shape)
-    # print(u, s, vt.T)
-    # print(np.diag(s, 1)[:-1,1:])
-    # u, s, vt = np.linalg.svd(true[cols[1:]].to_numpy().tolist(), full_matrices=False)
-    # print(np.diag(s))
+    scaler = normalizer.standard_scaler(data[cols[1:]])
+    scaled = scaler.transform(data[cols[1:]])
+    
+    print(true.corr(method='pearson'))
+    print(data.corr(method='spearman'))
+    print(true.corr(method='kendall'))
 
-    # scaler = normalizer.standard_scaler(data[cols[1:]])
-    # scaled = scaler.transform(data[cols[1:]])
-    # inverse = transformer.inverse_transform(scaled)
+    _, _, lcl, ucl = mspc.tsquare_single(true[cols[1:]][8:], 4)
+    all_score, _, _, _ = mspc.tsquare_single(data[cols[1:]], 4)
+    decompose_score = mspc.tsquare_decomposition(data, cols[1:])
+    residual = mspc.tsquare_residual(all_score, decompose_score)
+    err_idx = mspc.ano_score_idx(all_score, lcl, ucl)
 
-    # print(data[cols[1:]])
-    # print(scaled)
-    # print(inverse)
-
-    # transformer = common.pca_transform(scaled, 2)
-    # pca_score = transformer.transform(scaled)
-    # inverse_score = transformer.inverse_transform(pca_score)
-    # print(scaled)
-    # print(inverse_score)
-    # print(scaled - inverse_score)
-    #
-    # print(scaler.inverse_transform(scaled))
-    # print(scaler.inverse_transform(inverse_score))
-    # print(scaler.inverse_transform(scaled) - scaler.inverse_transform(inverse_score))
-
-    # print(true.corr(method='pearson'))
-    # print(data.corr(method='spearman'))
-    # print(true.corr(method='kendall'))
-
-    # _, _, lcl, ucl = mspc.tsquare_single(true[cols[1:]][8:], 4)
-    # all_score, _, _, _ = mspc.tsquare_single(data[cols[1:]], 4)
-    # decompose_score = mspc.tsquare_decomposition(data, cols[1:])
-    # residual = mspc.tsquare_residual(all_score, decompose_score)
-    # err_idx = mspc.ano_score_idx(all_score, lcl, ucl)
-
-    # a, b, c, d = [], [], [], []
-    # for i in err_idx:
-    #     a.append(residual[0][i])
-    #     b.append(residual[1][i])
-    #     c.append(residual[2][i])
-    #     d.append(residual[3][i])
-    #
-    # print(a)
-    # print(b)
-    # print(c)
-    # print(d)
-    #
-    # import numpy as np
-    #
-    # print(np.sum(a), np.sum(b), np.sum(c), np.sum(d))
-    #
+    a, b, c, d = [], [], [], []
+    for i in err_idx:
+        a.append(residual[0][i])
+        b.append(residual[1][i])
+        c.append(residual[2][i])
+        d.append(residual[3][i])
+    
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    
+    import numpy as np
+    
+    print(np.sum(a), np.sum(b), np.sum(c), np.sum(d))
+    
     # _lcl = [lcl for i in range(all_score.size)]
     # _ucl = [ucl for i in range(all_score.size)]
 
